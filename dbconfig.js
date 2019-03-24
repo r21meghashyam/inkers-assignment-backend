bfff0094f4a6a70f189b4699e53f4d769df06e95
Meghashyam Bhandary <r21meghashyam@gmail.com>
2019-03-24 17:55:12 +0530

    udpated dbconfig values to production database

--- dbconfig.js
+++ dbconfig.js
@@ -9,8 +9,8 @@ module.exports = {
 */
 //production
 module.exports = {
-    host:"localhost",
-    database:"mydb",
-    user:"root",
-    password:"B3V0XuvG"
+    host:"eu-cdbr-west-02.cleardb.net",
+    database:"heroku_662e8cf4b43e2cb",
+    user:"b0fcdb47fd2e06",
+    password:"64898708"
 }
